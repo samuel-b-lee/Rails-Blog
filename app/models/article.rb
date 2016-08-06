d4d0c8f61dbf4adbf135e0ed49dcecc43eb91460
Sam Lee <sam.lee.dtb@gmail.com>
2016-08-05 18:51:25 -0700

    tag feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,23 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+		self.tags.collect do |tag|
+			tag.name
+		end.join(", ")
+	end
+
+	def tag_list=(tags_string)
+		tag_names = tags_string.split(",").collect{ |s| # 1. split tags individually
+			s.strip.downcase							# 2. strip whitespace and make all characters lower case
+		}.uniq											# 3. ensure each tag is unique
+
+		new_or_found_tags = tag_names.collect { |name|
+		 Tag.find_or_create_by(name: name) 
+		}
+
+  		self.tags = new_or_found_tags
+	end
 end
